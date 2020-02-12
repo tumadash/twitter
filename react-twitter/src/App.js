f1519f7b1b2f365490c88dbfa51b2a5b1179e7c3
tumadash <dasha.tumanova@inbox.ru>
2020-02-12 14:32:21 +0300

    Twitter. Регистрация.

--- react-twitter/src/App.js
+++ react-twitter/src/App.js
@@ -7,12 +7,14 @@ import {persistor, store} from "./store/store";
 import {Provider} from 'react-redux'
 import {PersistGate} from 'redux-persist/integration/react';
 import {SignIn} from "./containers/SignIn";
+import SignUp from "./containers/SignUp";
 
 export const App = () => {
     return <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
             <HashRouter>
                 <Route exact path='/' component={SignIn}/>
+                <Route exact path='/registration' component={SignUp}/>
                 <Route path='/main' component={Main}/>
             </HashRouter>
         </PersistGate>
---
+++ react-twitter/src/components/Copyright.js
@@ -0,0 +1,16 @@
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import React from "react";
+
+export const Copyright = () => {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                tumadash
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+};
