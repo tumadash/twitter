b83f08f9a7fec72fa22dd499d18369e8f7eb9ec8
tumadash <dasha.tumanova@inbox.ru>
2020-02-16 20:42:12 +0300

    Twitter. Добавление постов.

--- react-twitter/src/components/News.js
+++ react-twitter/src/components/News.js
@@ -4,18 +4,26 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import {makeStyles} from "@material-ui/core/styles";
 import NewPost from "./NewPost";
-import SaveIcon from "@material-ui/core/SvgIcon/SvgIcon";
+import AddRoundedIcon from '@material-ui/icons/AddRounded';
 import Button from "@material-ui/core/Button";
 import {setOpen} from "../store/newPostState/actions";
 import {connect} from "react-redux";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(() => ({
     empty: {
         display: 'flex',
         color: '#757575',
         height: 'calc(100vh - 64px)',
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    newPost: {
+        position: 'absolute',
+        right: '25px',
+        backgroundColor: '#3f51b5',
+        color: 'white',
+        borderRadius: '50%',
+        height: '60px'
     }
 }));
 const News = ({news, setOpen}) => {
@@ -25,9 +33,9 @@ const News = ({news, setOpen}) => {
         setOpen(true);
     };
     return (<div>
-        <Button className={classes.saveButton} variant={'contained'}
-                color={'primary'}
-                startIcon={<SaveIcon/>} onClick={handleClickOpen}>New post</Button>
+        <Button color="primary" aria-label="add an alarm" className={classes.newPost} onClick={handleClickOpen}>
+            <AddRoundedIcon fontSize={'large'}/>
+        </Button>
         <NewPost/>
         <Grid>
             {news.length !== 0 ? news.map((value) => (<Grid key={value}><RecipeReviewCard newsItem={value}/></Grid>)) :
