c4010ed7da44b592a04919e3f10bfbdeabd5b9ac
tumadash <dasha.tumanova@inbox.ru>
2020-02-17 22:54:54 +0300

    Twitter. Переход на профиль. Не апдейтится.

--- react-twitter/src/containers/Home.js
+++ react-twitter/src/containers/Home.js
@@ -2,11 +2,11 @@ import React from "react";
 import News from "../components/News";
 import {connect} from "react-redux";
 
-const Home = ({news, currentUser}) => {
+const Home = ({news, currentUser, setProfileUser}) => {
     const userNews = news.filter(
         news => news.user.email === currentUser.email
     );
-    return (<News news={userNews}/>);
+    return (<News news={userNews} setProfileUser={setProfileUser}/>);
 };
 const mapStateToProps = state => ({
     news: state.news,
